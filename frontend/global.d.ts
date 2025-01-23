0bb3ee2d8b0eaaee31da165ef04e269480fe6a21
unknown <anthonythapa@hotmail.com>
2025-01-23 20:22:46 +0000

    remove create-react-app bootstrap

--- frontend/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    Sketchfab: any; // Define 'any' type or replace with a more specific type
-  }
-}
-
-export {};  // Keep the file as a module to avoid conflicts
--- frontend/src/Services/auth.ts
+++
@@ -1,4 +0,0 @@
-// mock validation at the moment
-export const authenticate = (nickname: string) => {
-  return nickname.trim() !== '';
-}
