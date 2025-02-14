9aff60cc26d655886ec464bae49dacbc4e4646d3
unknown <anthonythapa@hotmail.com>
2025-02-14 17:14:28 +0000

    provide session and real-time context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router';
-import { UserProvider } from './Context/useAuth';
+import { SignalRProvider } from './Context/useSignalR';
+import { SessionProvider } from './Context/useSession';
 
 function App() {
   return(
-    <UserProvider>
-      <div className='min-h-screen bg-[#0e1115] text-[#e9eef2]'>
-        <Outlet/>
-      </div>
-    </UserProvider>
+    <SignalRProvider>
+      <SessionProvider>
+        <div className='min-h-screen bg-[#0e1115] text-[#e9eef2]'>
+          <Outlet/>
+        </div>
+      </SessionProvider>
+    </SignalRProvider>
   )
 }
 
