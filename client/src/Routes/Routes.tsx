5b9a9e9de2b5b15fb5d81cb265994191e8f6afeb
unknown <anthonythapa@hotmail.com>
2025-02-14 17:15:16 +0000

    update paths

--- client/src/Routes/Routes.tsx
+++ client/src/Routes/Routes.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
-import ChatRoom from "../Pages/ChatRoom";
-import Login from "../Pages/Login";
+import Home from "../Pages/Home";
+import Draw from "../Containers/Draw";
+import Chat from "../Containers/Chat";
+import Room from "../Pages/Room";
 
 export const router = createBrowserRouter(
 [{
   path: '/',
   element: <App/>,
   children: [
-    {path: '', element: <Login />},
-    {path: 'chat', element: <ChatRoom />},
+    {path: '', element: <Home />},
+    {path: 'chat', element: <Chat />},
+    {path: 'draw', element: <Draw/>},
+    {path: 'room', element: <Room/>},
   ]
 }]);
