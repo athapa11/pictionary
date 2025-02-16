205b1bb7afdac85451b86fc49043e0666a8c1bd9
unknown <anthonythapa@hotmail.com>
2025-02-16 00:36:14 +0000

    removed scores dictionary

--- server/Models/Session.cs
+++ server/Models/Session.cs
@@ -8,6 +8,5 @@ namespace server.Models
     public string CurrentWord { get; set; } = string.Empty;
     public string DrawerId { get; set; } = string.Empty;
     public ConcurrentDictionary<string, Player> Players { get; set; } = new(); // PlayerId => Name
-    // public ConcurrentDictionary<string, int> Scores { get; set; } = new(); // PlayerId -> Score
 	}
 }
