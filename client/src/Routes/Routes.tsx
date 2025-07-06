be3255c9c0ae8e5122807a88f92291e908e48f69
unknown <anthonythapa@hotmail.com>
2025-07-06 01:29:52 +0100

    cleanup: removed redundant routes

--- client/src/Routes/Routes.tsx
+++ client/src/Routes/Routes.tsx
@@ -1,10 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../Pages/Home";
-import Draw from "../Containers/Draw";
-import Chat from "../Containers/Chat";
 import Room from "../Pages/Room";
-import Join from "../Pages/Join";
 
 export const router = createBrowserRouter(
 [{
@@ -12,9 +9,6 @@ export const router = createBrowserRouter(
   element: <App/>,
   children: [
     { path: '', element: <Home/> },
-    { path: 'chat', element: <Chat/> },
-    { path: 'draw', element: <Draw/> },
-    { path: 'room/:sessionId', element: <Room/> },
-    { path: 'join', element: <Join/> }
+    { path: 'room', element: <Room/> }
   ]
 }]);
